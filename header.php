8237a3644dc687ef67decf1ec88736784c3831e9
minohalberd <50263171+minohalberd@users.noreply.github.com>
2020-04-08 18:10:13 -0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Registradora</title>
+    <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css">
+    <!-- FONT AWESOME VERSION 5 -->
+    <script src="https://kit.fontawesome.com/cf800c27b9.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+<!-- <nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+        <a href="index.html" class="navbar-brand">Registradora</a>
+    </div>
+</nav> -->
